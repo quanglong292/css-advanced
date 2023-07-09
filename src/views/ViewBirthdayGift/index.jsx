1568f4e651d2d5b4a0e0ae4d0e9b6ccc21219d6c
quanglong292 <quang.long292@gmail.com>
2023-07-09 20:07:41 +0700

    birthday gift

---
+++ src/views/ViewBirthdayGift/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./birthDay.scss";
+
+const ViewBirthdayGift = () => {
+  return (
+    <>
+      <div class="wrapper">
+        <div class="lid one"></div>
+        <div class="lid two"></div>
+        <div class="envelope"></div>
+        <div class="letter cursor-pointer">
+          <p>Hello</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ViewBirthdayGift;
